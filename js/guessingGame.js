2744404e226d7d0a806d2d7f4e47e68b7fa859ac
Drew Vartanian <Drew.Vartanian@gmail.com>
2015-07-14 14:54:47 -0400

    Changed javascript to contain game data and functions in a game object

--- js/guessingGame.js
+++ js/guessingGame.js
@@ -5,48 +5,68 @@
 
 //Generate the game and controls on document ready
 $(document).ready(function(){
-	genTarget();
-	$('.tryAgain').on('click',tryAgain);
-	$('#guessingGameboard').on('click','#hint',hint);
-	$('#guessingGameboard').on('keypress','#guessInput',guessCharFilter);
-	$('#guessingGameboard').on('keydown','#guessInput',guessInputEnter);
-	$('#guessingGameboard').on('submit','#guessForm',guess);
+	game = new GameObj();
+	
+	$('.tryAgain').on('click',function(){
+		game.tryAgain();
+	});
+	$('#guessingGameboard').on('click','#hint',function(){
+		game.hint();
+	});
+	$('#guessingGameboard').on('keypress','#guessInput',function(event){
+		var input = this;
+		game.guessCharFilter(event,input);
+	});
+	$('#guessingGameboard').on('keydown','#guessInput',function(event){
+		var input = this;
+		game.guessInputEnter(event,input);
+	});
+	$('#guessingGameboard').on('submit','#guessForm',function(event){
+		var form = this;
+		game.guess(event,form);
+	});
 
 });
 
-var TEMP_DESC = ['Super Hot','Hot','Warm','Cold','Ice Cold'];  //Should have 5 entries ordered from most accurate to least accurate
-var TEMP_DIFFS = [5,10,15,25];                                 //Should have 4 entries ordered from lowest to highest
-var INPUT_RANGE = [1,100];                                     //Should have 2 entries ordered from lowest to highest
+var game;  //The game object
 
-var target;					//Game's target number
-var guessHistory;			//History of numbers guessed in the current game
-var hintText;				//The text to be displayed when a hint is asked for;
-var wonGame;				//Has the game been won yet?
+function GameObj(){
+	this.TEMP_DESC = ['Super Hot','Hot','Warm','Cold','Ice Cold'];  //Should have 5 entries ordered from most accurate to least accurate
+	this.TEMP_DIFFS = [5,10,15,25];									 //Should have 4 entries ordered from lowest to highest
+	this.INPUT_RANGE = [1,100];										 //Should have 2 entries ordered from lowest to highest
+	
+	this.targetVal;			//Game's target number
+	this.guessHistory;		//History of numbers guessed in the current game
+	this.hintText;			//The text to be displayed when a hint is asked for;
+	this.wonGame;			//Has the game been won yet?
 
+	this.genTarget();
+}
 
 //Generates the game
-function genTarget(){
-	target = Math.floor(Math.random()*INPUT_RANGE[1])+INPUT_RANGE[0];
-	guessHistory = [];
-	hintText= 'Pst! Try '+target+'!';
+GameObj.prototype.genTarget = function(){
+	this.targetVal = Math.floor(Math.random()*this.INPUT_RANGE[1])+this.INPUT_RANGE[0];
+	this.guessHistory = [];
+	this.hintText= 'Pst! Try '+this.targetVal+'!';
+	this.wonGame=false;
+
 	$('#guessingGameboard').find('#main-text').text('Can You Guess my Number?');
 	$('#guessingGameboard').find('#sub-text').text("I'm thinking of a number between 1 and 100.  You have five guesses.");
 	$('#guessingGameboard').find('#gameScreen').addClass('start');
 	$('#guessingGameboard').find('#fire').addClass('glyphicon glyphicon-fire fire-cold');
-	wonGame=false;
 }
 
 //Clears the board and generates a new game
-function tryAgain(){
+GameObj.prototype.tryAgain = function(){
 	$('#game-modal').modal('hide');
 	$('#guessingGameboard').find('.jumbotron').attr({'class':'jumbotron'});
 	$('#guessingGameboard').find('#fire').removeClass();
 	$('#guessingGameboard').find('#guessInput').val('');
-	genTarget();
-}
+	this.genTarget();
+};
 
 //Handels the modal
-function modalMessage(text,showTryAgain){
+GameObj.prototype.modalMessage = function(text,showTryAgain){
 	if(showTryAgain){
 		$('#modal-try-again').removeClass('invisible');
 		$('#modal-try-again').addClass('visible');
@@ -56,15 +76,15 @@ function modalMessage(text,showTryAgain){
 	}
 	$('#game-modal').find('#targetHint').text(text);
 	$('#game-modal').modal('show');
-}
+};
 
 //Calls for the hint modal
-function hint(){
-	modalMessage(hintText,false);
-}
+GameObj.prototype.hint = function(){
+	this.modalMessage(this.hintText,false);
+};
 
 //Attempts to filter input to only allow integers between 1-100
-function guessCharFilter(event){
+GameObj.prototype.guessCharFilter = function(event,input){
 	
 	var val;
 	var valNew;
@@ -74,55 +94,55 @@ function guessCharFilter(event){
 		return;
 	}
 
-	val = +$(this).val();
+	val = +$(input).val();
 	setTimeout($.proxy(function() {
-    	valNew = +$(this).val();
-    	if(valNew>INPUT_RANGE[1]){
+    	valNew = +$(input).val();
+    	if(valNew>this.INPUT_RANGE[1]){
     		if(val===0){
-    			$(this).val('');
+    			$(input).val('');
     		}
-    		$(this).val(val);
+    		$(input).val(val);
     	}
     	if(valNew===0){
-    		$(this).val('');
+    		$(input).val('');
     	}
   	}, this), 1);
-}
+};
 
 //Submits the input on enter
-function guessInputEnter(event){
+GameObj.prototype.guessInputEnter = function(event,input){
 	if(event.keyCode===13){
-		$(this).closest('form').submit();
+		$(input).closest('form').submit();
 	}
-}
+};
 
 //Handles a winning guess
-function win(){
+GameObj.prototype.win = function(){
 	var jumbotron = $('#guessingGameboard').find('.jumbotron');
 	var fire = $('#guessingGameboard').find('#fire');
 	$('#guessingGameboard').find('#main-text').text('Congrats! You Win!');
-	$('#guessingGameboard').find('#sub-text').text('You Figured Out I was Thinking of '+target+'!');
+	$('#guessingGameboard').find('#sub-text').text('You Figured Out I was Thinking of '+this.targetVal+'!');
 	fire.removeClass();
 	fire.addClass('glyphicon glyphicon-fire fire-win');
 	jumbotron.removeClass();
 	jumbotron.addClass('jumbotron win');
-	wonGame=true;
-}
+	this.wonGame=true;
+};
 
 //Handles a loosing guess
-function lose(){
+GameObj.prototype.lose = function(){
 	var jumbotron = $('#guessingGameboard').find('.jumbotron');
 	var fire = $('#guessingGameboard').find('#fire');
 	$('#guessingGameboard').find('#main-text').text('You Lose!');
-	$('#guessingGameboard').find('#sub-text').text('It was '+target+'. Better Luck Next Time!');
+	$('#guessingGameboard').find('#sub-text').text('It was '+this.targetVal+'. Better Luck Next Time!');
 	fire.removeClass();
 	fire.addClass('glyphicon glyphicon-fire fire-cold');
 	jumbotron.removeClass();
 	jumbotron.addClass('jumbotron i-cold');
-}
+};
 
 //Handels a wrong guess
-function wrongGuess(value){
+GameObj.prototype.wrongGuess = function(value){
 	var temp;
 	var tempClass;
 	var diff;
@@ -130,28 +150,28 @@ function wrongGuess(value){
 	var gameScreen = $('#guessingGameboard').find('#gameScreen');
 	var fire = gameScreen.find('#fire');
 
-	if(value<target){
+	if(value<this.targetVal){
 		direction = 'Higher';
 	}else{
 		direction = 'Lower';
 	}
 
 	
-	diff=Math.abs(value-target);
+	diff=Math.abs(value-this.targetVal);
 
-	if(diff<=TEMP_DIFFS[0]){
+	if(diff<=this.TEMP_DIFFS[0]){
 		temp=0;
-	}else if(diff<=TEMP_DIFFS[1]){
+	}else if(diff<=this.TEMP_DIFFS[1]){
 		temp=1;
-	}else if(diff<=TEMP_DIFFS[2]){
+	}else if(diff<=this.TEMP_DIFFS[2]){
 		temp=2;
-	}else if(diff<=TEMP_DIFFS[3]){
+	}else if(diff<=this.TEMP_DIFFS[3]){
 		temp=3;
 	}else{
 		temp=4;
 	}
 
-	$('#guessingGameboard').find('#main-text').text('You Are '+TEMP_DESC[temp]+'!');
+	$('#guessingGameboard').find('#main-text').text('You Are '+this.TEMP_DESC[temp]+'!');
 	$('#guessingGameboard').find('#sub-text').text('You Tried '+value+', Now Try '+direction+'!');
 	switch (temp) {
 		case 0:
@@ -178,53 +198,53 @@ function wrongGuess(value){
 	fire.removeClass();
 	fire.addClass('glyphicon glyphicon-fire fire-'+tempClass);
 
-}
+};
 
 //Recieves and handles all guesses that are submited
-function guess(event){
-	var val = +$(this).find('#guessInput').val();
+GameObj.prototype.guess = function(event,form){
+	var val = +$(form).find('#guessInput').val();
 
-	if(wonGame){
-		modalMessage('Congrats! Think you can do it again?',true);
+	if(this.wonGame){
+		this.modalMessage('Congrats! Think you can do it again?',true);
 		event.preventDefault();
 		return;
 	}
 
-	if(guessHistory.length>=5){
-		modalMessage('What to give it another shot?',true);
+	if(this.guessHistory.length>=5){
+		this.modalMessage('What to give it another shot?',true);
 		event.preventDefault();
 		return;
 	}
 
-	if(val > INPUT_RANGE[1] || val < INPUT_RANGE[0] || isNaN(val)){
-		modalMessage('You might have better luck trying an integer between 1 and 100!',false);
+	if(val > this.INPUT_RANGE[1] || val < this.INPUT_RANGE[0] || isNaN(val)){
+		this.modalMessage('You might have better luck trying an integer between 1 and 100!',false);
 		event.preventDefault();
 		return;
 	}
 
-	if(guessHistory.indexOf(val)!=-1){
-		modalMessage('You already guessed '+val+"!  Why don't you try something else?",false);
+	if(this.guessHistory.indexOf(val)!=-1){
+		this.modalMessage('You already guessed '+val+"!  Why don't you try something else?",false);
 		event.preventDefault();
 		return;
 	}
 
-	guessHistory.push(val);
+	this.guessHistory.push(val);
 
-	if(val==target){
-		win();
+	if(val==this.targetVal){
+		this.win();
 		event.preventDefault();
 		return;
 	}
 
-	if(guessHistory.length===5){
-		lose();
+	if(this.guessHistory.length===5){
+		this.lose();
 		event.preventDefault();
 		return;
 	}
 
-	wrongGuess(val);
+	this.wrongGuess(val);
 
 	$('#guessingGameboard').find('#guessInput').val('');
 
 	event.preventDefault();
-}
+};
